80c681fb1e76ade656943ee55b36bad3bff2db4c
mhbauleo <mhbfiuba@gmail.com>
2022-04-04 23:51:04 -0300

    cambio a confirmarPermisos

--- routes/apiProductos.js
+++ routes/apiProductos.js
@@ -56,7 +56,10 @@ function confirmarPermisos(req, res, next) {
   if (admin) {
     next();
   } else {
-    res.json({ error: -1, descripcion: `ruta /api/productos método ${req.method} no autorizada` });
+    res.json({
+      error: -1,
+      descripcion: `ruta /api/productos${req.path} método ${req.method} no autorizada`,
+    });
   }
 }
 
